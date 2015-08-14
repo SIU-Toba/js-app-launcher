e77523ff0a31c278b477c669d4a7d3c94f2c3797
fbohnsiu <fbohn@siu.edu.ar>
2015-08-14 16:41:34 -0300

    Ajustes en la libreria. Se pasa un parametro con todas las configuraciones para la inicializacion.

--- app_launcher.js
+++ app_launcher.js
@@ -1,114 +1,147 @@
 // Class appLauncher
 var appLauncher = new function () {
-
-    var scroll = false;
-    var launcherMaxLineHeight = 100;
     
     // Metodo que inicializa el lanzador de aplicaciones
-    this.init = function (container_appLauncher, appLauncherDataJSON, js_salir) {
+    this.init = function (appLauncherDataParam) {
         
-        // convertir el json string a array
-        var appLauncherData = jQuery.parseJSON(appLauncherDataJSON);
+        // variable que contiene los datos por defecto del appLauncher
+        var appLauncherDataDefault = {
+            launcherMaxLineHeight : 100,
+        };
+        
+        // variable que contiene los datos del appLauncher
+        var appLauncherData = $.extend({}, appLauncherDataDefault, appLauncherDataParam); // determino los datos del appLauncher
+        
+        // Variable para activar el scroll vertical
+        var scroll = false;
         // determino la cantidad de aplicaciones
-        var cant_apps = appLauncherData.aplicaciones.length;
+        var cant_apps = appLauncherData.data.aplicaciones.length;
         // determino la cantidad de lineas
         var cant_lineas = Math.ceil(cant_apps / 3);
-        
+        // variable que contiene el alto del primer contenedor de aplicaciones
         var height_first_set = 0;
+        // variable que contiene el alto total del contenedor de aplicaciones
         var height_apps = 0;
         
-        // Armo el HTML
-        
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
         // Armo el HTML del perfil de usuario
-        $(container_appLauncher).append("<div class='container_datos_usuario'> </div>");
-        $(".container_datos_usuario").html("<div class='datos_usuario_general'> </div>");
-        $(".datos_usuario_general").html("<div class='button_datos_usuario'> </div>");
-        $(".datos_usuario_general").append("<div class='datos_usuario'> </div>");
-        $(".datos_usuario").html("<div class='perfil_usuario'> </div>");
-        $(".perfil_usuario").html("<div class='perfil_usuario_foto'> </div>");
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
         
-        if (appLauncherData.usuario_foto != undefined) {
-            $(".button_datos_usuario").append("<div class='button_datos_usuario_foto'> <img class='perfil_usuario_boton_foto_img' src='" + appLauncherData.usuario_foto + "'></i> </div>");
-            $(".perfil_usuario_foto").html("<img class='perfil_usuario_foto_img' src='" + appLauncherData.usuario_foto + "'></i> ");
+        var html_usuario = "";
+        html_usuario += "<div id='container_datos_usuario'>";
+        html_usuario += "   <div id='datos_usuario_general'>";
+        html_usuario += "       <div id='button_datos_usuario'>";
+        html_usuario += "           <div id='button_datos_usuario_foto'>";
+        html_usuario += "           </div>";
+        html_usuario += "       </div>";
+        html_usuario += "       <div id='datos_usuario'>";
+        html_usuario += "           <div id='perfil_usuario'>";
+        html_usuario += "               <div id='perfil_usuario_foto'>";
+        html_usuario += "               </div>";
+        html_usuario += "               <div id='perfil_usuario_cuenta'>";
+        html_usuario += "                   <div id='perfil_usuario_cuenta_nombre'>";
+        html_usuario += "                   </div>";
+        html_usuario += "                   <div id='perfil_usuario_cuenta_id'>";
+        html_usuario += "                   </div>";
+        html_usuario += "                   <div id='perfil_usuario_cuenta_botones'>";
+        html_usuario += "                       <div id='perfil_usuario_cuenta_perfil'>";
+        html_usuario += "                       </div>";
+        html_usuario += "                       <div id='perfil_usuario_cuenta_salir'>";
+        html_usuario += "                       </div>";
+        html_usuario += "                   </div>";
+        html_usuario += "               </div>";
+        html_usuario += "           </div>";
+        html_usuario += "       </div>";
+        html_usuario += "   </div>";
+        html_usuario += "</div>";
+        $(appLauncherData.container).append(html_usuario);
+                
+        if (appLauncherData.data.usuario_foto != undefined) {
+            $(appLauncherData.container + " #button_datos_usuario_foto").html("<img id='perfil_usuario_boton_foto_img' src='" + appLauncherData.data.usuario_foto + "'></i>");
+            $(appLauncherData.container + " #perfil_usuario_foto").html("<img id='perfil_usuario_foto_img' src='" + appLauncherData.data.usuario_foto + "'></i> ");
         }
-        $(".button_datos_usuario").append("<div class='perfil_usuario_boton_cuenta_id'>" + appLauncherData.usuario_id + "</div>");
         
-        $(".perfil_usuario").append("<div class='perfil_usuario_cuenta'> </div>");
-        $(".perfil_usuario_cuenta").html("<div class='perfil_usuario_cuenta_nombre'> </div>");
-        
-        if (appLauncherData.usuario_nombre != undefined) {
-            $(".perfil_usuario_cuenta_nombre").html(appLauncherData.usuario_nombre);
+        if (appLauncherData.data.usuario_nombre != undefined) {
+            $(appLauncherData.container + " #perfil_usuario_cuenta_nombre").html(appLauncherData.data.usuario_nombre);
         }
         
-        $(".perfil_usuario_cuenta").append("<div class='perfil_usuario_cuenta_id'> </div>");
-        if (appLauncherData.usuario_id != undefined) {
-            $(".perfil_usuario_cuenta_id").html(appLauncherData.usuario_id);
+        if (appLauncherData.data.usuario_id != undefined) {
+            $(appLauncherData.container + " #button_datos_usuario").append("<div id='perfil_usuario_boton_cuenta_id'>" + appLauncherData.data.usuario_id + "</div>");
+            $(appLauncherData.container + " #perfil_usuario_cuenta_id").html(appLauncherData.data.usuario_id);
         }
         
-        $(".perfil_usuario_cuenta").append("<div class='perfil_usuario_cuenta_botones'> </div>");
-        
-        $(".perfil_usuario_cuenta_botones").append("<div class='perfil_usuario_cuenta_perfil'> </div>");
-        if (appLauncherData.perfil_url != undefined) {
-            $(".perfil_usuario_cuenta_perfil").html("<a class='boton_cuenta' href='"+ appLauncherData.perfil_url + "' target='perfil_usuario_" + appLauncherData.usuario_id + "'> Mi cuenta  </a>" );
+        if (appLauncherData.data.perfil_url != undefined) {
+            $(appLauncherData.container + " #perfil_usuario_cuenta_perfil").html("<a id='boton_cuenta' href='"+ appLauncherData.data.perfil_url + "' target='perfil_usuario_" + appLauncherData.data.usuario_id + "'> Mi cuenta  </a>" );
         }
         
-        $(".perfil_usuario_cuenta_botones").append("<div class='perfil_usuario_cuenta_salir'> </div>");
-        if (js_salir != undefined) {
-            $(".perfil_usuario_cuenta_salir").html("<a class='boton_salir' href='#' onclick = '" + js_salir + "'> Salir </a>" );
+        if (appLauncherData.js_salir != undefined) {
+            $(appLauncherData.container + " #perfil_usuario_cuenta_salir").html("<a id='boton_salir' href='#'> Salir </a>" );
+            $(appLauncherData.container + ' #boton_salir').click(appLauncherData.js_salir);
         }
         
         // Armo la logica del boton de perfil del usuario
-        this.setearLogicaBoton('.datos_usuario', '.button_datos_usuario', ['.app-launcher']);
+        this.setearLogicaBoton(appLauncherData.container + ' #datos_usuario', appLauncherData.container + ' #button_datos_usuario', [appLauncherData.container + ' #app-launcher']);
          
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
         // Armo el HTML con las aplicaciones
-        if (cant_apps > 0) {
-            $(container_appLauncher).append("<div class='container_aplicaciones'> </div>");
-            $(".container_aplicaciones").html("<div class='launcher'> </div>");
-            $(".launcher").html("<div class='button'><i class='fa fa-th fa-2x'></i></div>");
-            $(".launcher").append("<div class='app-launcher'> </div>");
-            $(".app-launcher").html("<div class='app-launcher-container'> </div>");
-            $(".app-launcher-container").append("<div class='apps'> </div>");
+        //////////////////////////////////////////////////////////////////////////////////////////////////////
         
-            $(".apps").html("<ul class='first-set'> </ul>");
-
+        if (cant_apps > 0) {
+            var html_aplicaciones = "";
+            html_aplicaciones += "  <div id='container_aplicaciones'>";
+            html_aplicaciones += "      <div id='launcher'>";
+            html_aplicaciones += "          <div id='button'><i class='fa fa-th fa-2x'></i>";
+            html_aplicaciones += "          </div>";
+            html_aplicaciones += "          <div id='app-launcher'>";
+            html_aplicaciones += "              <div id='app-launcher-container'>";
+            html_aplicaciones += "                  <div id='apps'>";
+            html_aplicaciones += "                      <ul id='first-set'>";
+            html_aplicaciones += "                      </ul>";
+            html_aplicaciones += "                  </div>";
+            html_aplicaciones += "              </div>";
+            html_aplicaciones += "          </div>";
+            html_aplicaciones += "      </div>";
+            html_aplicaciones += "  </div>";
+            $(appLauncherData.container).append(html_aplicaciones);
+            
             if (cant_lineas <=3) {
-                height_first_set = cant_lineas * launcherMaxLineHeight;
+                height_first_set = cant_lineas * appLauncherData.launcherMaxLineHeight;
                 height_apps = height_first_set + 57;
             } else {
-                height_first_set = 3 * launcherMaxLineHeight;
+                height_first_set = 3 * appLauncherData.launcherMaxLineHeight;
                 height_apps = height_first_set + 97;
 
-                $(".apps").append("<a href='#' class='more'>Más</a>");
-                $(".apps").append("<ul class='second-set hide'> </div>");
+                $(appLauncherData.container + " #apps").append("<a href='#' id='more'>Más</a>");
+                $(appLauncherData.container + " #apps").append("<ul id='second-set' class='hide_app_launcher'> </div>");
             }
 
             // Recorro las aplicaciones y generon los links
-            $(appLauncherData.aplicaciones).each(function( index, element ) {
+            $(appLauncherData.data.aplicaciones).each(function( index, element ) {
                 if (element.url != undefined && element.icono_url != undefined && element.etiqueta != undefined && element.title != undefined) {
                     if (index < 9) {
-                        $(".first-set").append("<li> <a class='link_aplicaciones' href='"+ element.url +"' target='aplicacion_"+ index +"' id='aplicacion_"+ index +"'> <div> <img class='fa fa-4x icono_url' src='" + element.icono_url + "' alt='" + element.title + "'> </i> </div> <div> " + element.etiqueta + " </div> </a> </li>");
+                        $(appLauncherData.container + " #first-set").append("<li> <a class='link_aplicaciones' href='"+ element.url +"' target='aplicacion_"+ index +"' id='aplicacion_"+ index +"'> <div> <img class='fa fa-4x icono_url' src='" + element.icono_url + "' alt='" + element.title + "'> </i> </div> <div> " + element.etiqueta + " </div> </a> </li>");
                     } else {
-                        $(".second-set").append("<li> <a class='link_aplicaciones' href='"+ element.url +"' target='aplicacion_"+ index +"' id='aplicacion_"+ index +"'> <div> <img class='fa fa-4x icono_url' src='" + element.icono_url + "' alt='" + element.title + "'></i> </div> <div> " + element.etiqueta + " </div> </a> </li>");
+                        $(appLauncherData.container + " #second-set").append("<li> <a class='link_aplicaciones' href='"+ element.url +"' target='aplicacion_"+ index +"' id='aplicacion_"+ index +"'> <div> <img class='fa fa-4x icono_url' src='" + element.icono_url + "' alt='" + element.title + "'></i> </div> <div> " + element.etiqueta + " </div> </a> </li>");
                     }
                 }
             });
 
             // Setea el maximo alto del contenedor de items
-            $('.first-set').css({height: height_first_set});
+            $(appLauncherData.container + ' #first-set').css({height: height_first_set});
                 
             // Setea el maximo alto del contenedor de aplicaciones
-            $('.apps').css({height: height_apps});
+            $(appLauncherData.container + ' #apps').css({height: height_apps});
 
             // Mousewheel event handler to detect whether user has scrolled over the container
-            $('.apps').bind('mousewheel', function (e) {
+            $(appLauncherData.container + ' #apps').bind('mousewheel', function (e) {
                 if (e.originalEvent.wheelDelta / 120 > 0) {
                     // Scrolling up
                 }
                 else {
                     // Scrolling down
                     if (!scroll) {
-                        $(".second-set").show();
-                        $('.apps').css({height: height_apps}).addClass('overflow');
+                        $(appLauncherData.container + " #second-set").show();
+                        $(appLauncherData.container + ' #apps').css({height: height_apps}).addClass('overflow');
                         scroll = true;
                         $(this).scrollTop(e.originalEvent.wheelDelta);
                     }
@@ -116,24 +149,37 @@ var appLauncher = new function () {
             });
 
             // Scroll event to detect that scrollbar reached top of the container
-            $('.apps').scroll(function () {
+            $(appLauncherData.container + ' #apps').scroll(function () {
                 var pos = $(this).scrollTop();
                 if (pos == 0) {
                     scroll = false;
-                    $('.apps').css({height: height_apps}).removeClass('overflow');
-                    $(".second-set").hide();
+                    $(appLauncherData.container + ' #apps').css({height: height_apps}).removeClass('overflow');
+                    $(appLauncherData.container + " #second-set").hide();
                 }
             });
 
             // Click event handler to show more apps
-            $('.apps .more').click(function () {
-                $(".second-set").show();
-                $(".apps").animate({scrollTop: $('.apps')[0].scrollHeight}).css({height: height_apps}).addClass('overflow');
+            $(appLauncherData.container + ' #apps #more').click(function () {
+                $(appLauncherData.container + " #second-set").show();
+                $(appLauncherData.container + " #apps").animate({scrollTop: $(appLauncherData.container + ' #apps')[0].scrollHeight}).css({height: height_apps}).addClass('overflow');
             }); 
 
             // Armo la logica del boton de aplicaciones
-            this.setearLogicaBoton('.app-launcher', '.button', ['.datos_usuario']);
+            this.setearLogicaBoton(appLauncherData.container + ' #app-launcher', appLauncherData.container + ' #button', [appLauncherData.container + ' #datos_usuario']);
         }
+        
+        // Metodo para ocultar appLauncher cuando se clickea fuera del board
+        $(document).click(function () {
+            //Hide the launcher if visible
+            $(appLauncherData.container + ' #app-launcher').hide();
+            $(appLauncherData.container + ' #datos_usuario').hide();
+        });
+
+        // Resize event handler to maintain the max-height of the app launcher
+        $(window).resize(function () {
+            $(appLauncherData.container + ' #apps').css({maxHeight: $(window).height() - $(appLauncherData.container + ' #apps').offset().top});
+            $(appLauncherData.container + ' #perfil_usuario').css({maxHeight: $(window).height() - $(appLauncherData.container + ' #perfil_usuario').offset().top});
+        });
         
     };
     
@@ -156,18 +202,5 @@ var appLauncher = new function () {
         $(divBoton).hide();
 
     };
-    
-    // Metodo para ocultar appLauncher cuando se clickea fuera del board
-    $(document).click(function () {
-        //Hide the launcher if visible
-        $('.app-launcher').hide();
-        $('.datos_usuario').hide();
-    });
-
-    // Resize event handler to maintain the max-height of the app launcher
-    $(window).resize(function () {
-        $('.apps').css({maxHeight: $(window).height() - $('.apps').offset().top});
-        $('.perfil_usuario').css({maxHeight: $(window).height() - $('.perfil_usuario').offset().top});
-    });
     
 };
